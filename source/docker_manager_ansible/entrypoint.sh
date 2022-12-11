f722f8f35ae5134041d5a15723d36ba932b0262f
Artyom <mr-coffey@protonmail.com>
2022-12-11 17:30:49 +1000

    fix: stupidity with keys

--- source/docker_manager_ansible/entrypoint.sh
+++ source/docker_manager_ansible/entrypoint.sh
@@ -4,13 +4,6 @@ set -euo pipefail
 
 SSH_HOST_RSA_KEY=${SSH_HOST_RSA_KEY:-}
 SSH_HOST_RSA_PUBLIC_KEY=${SSH_HOST_RSA_PUBLIC_KEY:-}
-
-SSH_HOST_ECDSA_KEY=${SSH_HOST_ECDSA_KEY:-}
-SSH_HOST_ECDSA_PUBLIC_KEY=${SSH_HOST_ECDSA_PUBLIC_KEY:-}
-
-SSH_HOST_ED25519_KEY=${SSH_HOST_ED25519_KEY:-}
-SSH_HOST_ED25519_PUBLIC_KEY=${SSH_HOST_ED25519_PUBLIC_KEY:-}
-
 SSH_AUTHORIZED_KEY=${SSH_AUTHORIZED_KEY:-}
 
 if [[ -n "${SSH_HOST_RSA_KEY}" ]]; then
@@ -18,7 +11,7 @@ if [[ -n "${SSH_HOST_RSA_KEY}" ]]; then
   chown root:root /etc/ssh/ssh_host_rsa_key
   chmod 0400 /etc/ssh/ssh_host_rsa_key
   unset SSH_HOST_RSA_KEY
-else
+elif [[ ! -f /etc/ssh/ssh_host_rsa_key ]]; then
   ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 fi
 
@@ -27,38 +20,6 @@ if [[ -n "${SSH_HOST_RSA_PUBLIC_KEY}" ]]; then
   chown root:root /etc/ssh/ssh_host_rsa_key.pub
   chmod 0400 /etc/ssh/ssh_host_rsa_key.pub
   unset SSH_HOST_RSA_PUBLIC_KEY
-fi
-
-if [[ -n "${SSH_HOST_ECDSA_KEY}" ]]; then
-  echo "${SSH_HOST_ECDSA_KEY}" >/etc/ssh/ssh_host_ecdsa_key
-  chown root:root /etc/ssh/ssh_host_ecdsa_key
-  chmod 0400 /etc/ssh/ssh_host_ecdsa_key
-  unset SSH_HOST_ECDSA_KEY
-else
-  ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
-fi
-
-if [[ -n "${SSH_HOST_ECDSA_PUBLIC_KEY}" ]]; then
-  echo "${SSH_HOST_ECDSA_PUBLIC_KEY}" >/etc/ssh/ssh_host_ecdsa_key.pub
-  chown root:root /etc/ssh/ssh_host_ecdsa_key.pub
-  chmod 0400 /etc/ssh/ssh_host_ecdsa_key.pub
-  unset SSH_HOST_ECDSA_PUBLIC_KEY
-fi
-
-if [[ -n "${SSH_HOST_ED25519_KEY}" ]]; then
-  echo "${SSH_HOST_ED25519_KEY}" >/etc/ssh/ssh_host_ed25519_key
-  chown root:root /etc/ssh/ssh_host_ed25519_key
-  chmod 0400 /etc/ssh/ssh_host_ed25519_key
-  unset SSH_HOST_ED25519_KEY
-else
-  ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
-fi
-
-if [[ -n "${SSH_HOST_ED25519_PUBLIC_KEY}" ]]; then
-  echo "${SSH_HOST_ED25519_PUBLIC_KEY}" >/etc/ssh/ssh_host_ed25519_key.pub
-  chown root:root /etc/ssh/ssh_host_ed25519_key.pub
-  chmod 0400 /etc/ssh/ssh_host_ed25519_key.pub
-  unset SSH_HOST_ED25519_PUBLIC_KEY
 fi
 
 if [[ -n "${SSH_AUTHORIZED_KEY}" ]]; then
