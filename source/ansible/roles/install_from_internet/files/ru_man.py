b41d4f54e983b427c7f669867bff0e9d357c9be4
Artyom <mr-coffey@protonmail.com>
2023-01-02 06:43:57 +1000

    fix: install ru mans (ref)

--- source/ansible/roles/install_from_internet/files/ru_man.py
+++ source/ansible/roles/install_from_internet/files/ru_man.py
@@ -20,29 +20,30 @@ def make_tarfile(output_file, source_dir):
     with tarfile.open(output_file, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
-def recursive_chown(path, user, group, rwx):
+def recursive_chown(path, user, group, rwx_dirs: str, rwx_files: str):
     for dir_path, dir_names, file_names in os.walk(path):
         shutil.chown(dir_path, user, group)
+        os.chmod(dir_path, int(rwx_dirs, base=8))
         for file_name in file_names:
             file = os.path.join(dir_path, file_name)
             shutil.chown(file, user, group)
-            os.chmod(file, int(rwx, base=8))
+            os.chmod(file, int(rwx_files, base=8))
 
 
 if __name__ == "__main__":
     link = get_link_for_download('https://sourceforge.net/projects/man-pages-ru/best_release.json')
     file = '/tmp/man_ru.tar.bz2'
     urllib.request.urlretrieve(link, file)
-    unpack_dir = os.path.join(os.path.dirname(file), 'man')
+    unpack_dir = os.path.join(os.path.dirname(file), 'man_ru')
     extract_bz2(file, unpack_dir)
     # например, man_dir = /tmp/man/man-pages-ru_5.03-2390-2390-20191017
-    man_dir = [f.path for f in os.scandir(unpack_dir) if f.is_dir()][0]
+    man_dir = os.path.join(unpack_dir, os.listdir(unpack_dir)[0])
     man_dir_system = "/usr/share/man/ru"  # без окончательного /
     backup_postfix = datetime.now().strftime("%d_%m_%y_(%H:%M:%S)")
     if os.path.isdir(man_dir_system):
         backup_file = os.path.join(os.path.dirname(man_dir_system), 'ru_' + backup_postfix + '.tar.gz')
         make_tarfile(backup_file, man_dir_system)
-    recursive_chown(man_dir, 'root', 'root', '644')
+    recursive_chown(man_dir, 'root', 'root', rwx_dirs='755', rwx_files='644')
     os.makedirs(man_dir_system, exist_ok=True)
     # shutil.copytree(man_dir, man_dir_system, dirs_exist_ok=True)  # 3.8+ only!
     # alt way with files
